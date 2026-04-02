2c3bda278dfeb41dbb5a73d9c095b51a2fa72787
LennixxQ <chauhanvivek1750@gmail.com>
2026-04-02 12:47:03 +0530

    Dashboard Controller Added and Tested Successfully

---
+++ HelpDesk.API/Controllers/DashboardController.cs
@@ -0,0 +1,29 @@
+﻿using HelpDesk.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDesk.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class DashboardController : ControllerBase
+    {
+        private readonly IDashboardService _dashboardService;
+        private readonly ILogger<DashboardController> _logger;
+
+        public DashboardController(IDashboardService dashboardService, ILogger<DashboardController> logger)
+        {
+            _dashboardService = dashboardService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDashboard()
+        {
+            _logger.LogInformation("Admin requesting dashboard data.");
+            var response = await _dashboardService.GetDashboardAsync();
+            return Ok(response);
+        }
+    }
+}
